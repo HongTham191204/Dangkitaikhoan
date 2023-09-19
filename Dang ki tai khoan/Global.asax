0141f5b3a771e5bade36822aeea804c59f52688f
Suzi161099 <Suzi161099@LAPTOP-MQTM9T7O>
2023-09-19 18:11:29 +0700

    Add project files.

---
+++ Dang ki tai khoan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dang_ki_tai_khoan.Global" Language="C#" %>
---
+++ Dang ki tai khoan/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Dang_ki_tai_khoan
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
